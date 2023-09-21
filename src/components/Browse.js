1b85023bd09e6995f236eec08d509af66f919e65
Ankit Sharma <ankitsharma290101@gmail.com>
2023-09-21 11:05:44 +0530

    Added GPTSearch

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -5,18 +5,23 @@ import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import { useSelector } from "react-redux";
+import GptSearch from "./GptSearch";
 
 const Browse = () => {
+  const showGptSearch = useSelector(store=>store.gpt.showGptSearch)
   useNowPlayingMovies()
   usePopularMovies()
   useTopRatedMovies()
   return( 
   <div>
   <Header />
+  { showGptSearch? <GptSearch/>:<>
   <MainContainer/>
   <SecondaryContainer/>
-  </div>)
-  ;
+  </>}
+  </div>);
+
 };
 
 export default Browse;
