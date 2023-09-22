b3e4cb4dcaaa38cb70ac2661b5e8e2cf4e8d5002
Ankit Sharma <ankitsharma290101@gmail.com>
2023-09-22 16:10:15 +0530

    Api hide in ENV file

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,9 +1,9 @@
 
 import OpenAI from 'openai';
-import { OPENAI_API } from './constants';
+// import { OPENAI_API } from './constants';
 
 const openai = new OpenAI({
-  apiKey: OPENAI_API,
+  apiKey: process.env.REACT_APP_OPENAI_API,
   dangerouslyAllowBrowser:true, // defaults to process.env["OPENAI_API_KEY"]
 });
 
