592bd92dc20267146798bfc35707bfe7519c5e5b
Ankit Sharma <ankitsharma290101@gmail.com>
2023-09-23 10:52:39 +0530

    Responsive

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -3,7 +3,8 @@ import OpenAI from 'openai';
 // import { OPENAI_API } from './constants';
 
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_API,
+  // apiKey: process.env.REACT_APP_OPENAI_API,
+  apiKey: "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",
   dangerouslyAllowBrowser:true, // defaults to process.env["OPENAI_API_KEY"]
 });
 
